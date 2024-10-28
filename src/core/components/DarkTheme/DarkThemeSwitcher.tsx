b8ca0e8c4a163f8544d0bb8f19bcf636a6f3635c
Miguel Vieira <miguel.vieira@ebo-enterprises.com>
2024-10-28 17:16:09 +0000

    feat: Persist Theme choice in localStorage

--- src/core/components/DarkTheme/DarkThemeSwitcher.tsx
+++ src/core/components/DarkTheme/DarkThemeSwitcher.tsx
@@ -1,29 +1,49 @@
-import { memo, useCallback, useMemo, useState } from "react";
+import { memo, useCallback, useEffect, useMemo, useState } from "react";
 import MoonIcon from '../../../svgs/moon.svg?react';
 import SunIcon from '../../../svgs/sun.svg?react';
 import Button from "../GenericButton/Button";
 import './DarkThemeSwitcher.scss';
 import classNames from "classnames";
 
+const themeStorageKey = "isDarkTheme";
+
 /**
- * Currently does not do much other than set a classname on the document.
+ * Currently does not do much other than set a `classname` on the document.
  */
 const DarkThemeSwitcher = () => {
-  const [isDarkTheme, setIsDarkTheme] = useState<boolean>(false);
-  const buttonTitle = useMemo(() => isDarkTheme ? 'Toggle Light Theme' : 'Toggle Dark Theme', [isDarkTheme]);
+  const [isDarkTheme, setIsDarkTheme] = useState<boolean>(() => {
+    return localStorage.getItem(themeStorageKey) === "true";
+  });
+
+  const buttonTitle = useMemo(
+    () => (isDarkTheme ? "Toggle Light Theme" : "Toggle Dark Theme"),
+    [isDarkTheme],
+  );
 
   const handleOnClick = useCallback(() => {
     const htmlTag = document.documentElement;
 
-    setIsDarkTheme(prevState => {
-      if (prevState) {
-        htmlTag.classList.remove('dark-theme');
+    setIsDarkTheme((prevState) => {
+      const newTheme = !prevState;
+      localStorage.setItem(themeStorageKey, newTheme.toString());
+
+      if (newTheme) {
+        htmlTag.classList.add("dark-theme");
       } else {
-        htmlTag.classList.add('dark-theme');
+        htmlTag.classList.remove("dark-theme");
       }
-      return !prevState;
+      return newTheme;
     });
   }, []);
+
+  useEffect(() => {
+    const htmlTag = document.documentElement;
+    if (isDarkTheme) {
+      htmlTag.classList.add("dark-theme");
+    } else {
+      htmlTag.classList.remove("dark-theme");
+    }
+  }, [isDarkTheme]);
 
   return (
     <Button
